8ce6c481d68bdb398f6140576624773b7d20c48b
hvlopez <hectorvmlopez@gmail.com>
2022-09-22 16:28:38 -0500

    guard on controllers

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,9 @@
 import { NestFactory } from '@nestjs/core';
-import { AuthGuard } from 'guards/auth.guard';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalGuards(new AuthGuard());
+
   await app.listen(3000);
 }
 bootstrap();
